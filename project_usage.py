f36c20eee470f42227af99ce357d9e9d130d9718
Nick Sjostrom <nsjostrom@dyn.com>
2019-05-24 16:25:28 -0400

    Create and retrieve people

--- project_usage.py
+++ project_usage.py
@@ -111,6 +111,10 @@ import requests
 import logging
 import argparse
 import base64
+from random import randint
+from faker import Faker
+from faker_schema.faker_schema import FakerSchema
+from faker_schema.schema_loader import load_json_from_file
 
 LOGMSG_TSFMT = '%a %b %d %H:%M:%S UTC %Y'
 LOGMSG_FORMAT = '%(asctime)s %(levelname)5s:%(filename)s:%(funcName)s:%(lineno)s: %(message)s'
@@ -125,35 +129,77 @@ logger.addHandler(stderr_handler)
 logger.setLevel(logging.INFO)
 logger.disabled = False
 
-accessToken = ""
-refreshToken = ""
+ACCESS_TOKEN = ""
+REFRESH_TOKEN = ""
 
 HOST = "https://{project}.machinable.io{path}"
 PATHS = {
-    "people": "/api/people",
+    "people": "/api/detailPeople",
     "count": "/api/count",
     "metrics": "/api/metrics",
     "login": "/sessions/",
     "refresh": "/sessions/refresh/",
 }
 
+SLEEP_TIME = 2
+
+fakerSchema = FakerSchema()
+fake = Faker()
+humanSchema = load_json_from_file('./schemas/people.details.json')
+
+class TokenExpired(Exception):
+  pass
+
 def login(project, username, password):
+    global ACCESS_TOKEN
+    global REFRESH_TOKEN
+    logger.info("logging in")
+
     encoded = base64.b64encode(bytes("{username}:{password}".format(username=username, password=password), 'utf8'))
     headers = {"Authorization": "Basic " + encoded.decode()}
     url = HOST.format(project=project, path=PATHS["login"])
     r = requests.post(url, headers=headers)
 
     response = r.json()
-    accessToken = response["access_token"]
-    refreshToken = response["refresh_token"]
+    ACCESS_TOKEN = response["access_token"]
+    REFRESH_TOKEN = response["refresh_token"]
 
-def refresh(token):
-    pass
+def refresh():
+    global ACCESS_TOKEN
+    
+    headers = {"Authorization": "Bearer " + REFRESH_TOKEN}
+    url = HOST.format(project=project, path=PATHS["refresh"])
+    r = requests.post(url, headers=headers)
+
+    if r.status_code == requests.codes.ok:
+      logger.info("refreshed token")
+      response = r.json()
+      ACCESS_TOKEN = response["access_token"]
+
+    return r.status_code
 
 def create_person(headers):
     data = generatePerson()
-    url = HOST.format(project=project, path=PATHS["login"])
-    r = requests.post(PROJECT_HOST + PATHS["API"], json = data, headers = {"Authorization": "apikey " + API_KEY})
+    url = HOST.format(project=project, path=PATHS["people"])
+    r = requests.post(url, json = data, headers = headers)
+
+    # TODO: be more specific with exceptions
+    if r.status_code != requests.codes.created:
+      logger.info("unexpected status code from create_person: {code}".format(code=r.status_code))
+      raise TokenExpired
+    
+    logger.info("created new person")
+
+def get_people(headers, limit, offset):
+    url = HOST.format(project=project, path=PATHS["people"])
+    r = requests.get(url, headers = headers)
+
+    # TODO: be more specific with exceptions
+    if r.status_code != requests.codes.ok:
+      logger.info("unexpected status code from get_people: {code}".format(code=r.status_code))
+      raise TokenExpired
+
+    logger.info("retrieved {limit} people offset by {offset}".format(limit=limit, offset=offset))
 
 def generatePerson():
     person = fakerSchema.generate_fake(humanSchema)
@@ -169,24 +215,13 @@ def generatePerson():
 
     return person
 
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create project usage (tests)")
-    parser.add_argument('project', help='The name of the project to use.')
-    parser.add_argument('--apikey', help='API Key to make authenticated requests, assumes read/write permissions.')
-    parser.add_argument('--username', help='Valid project username to login and make requests. Requires password.')
-    parser.add_argument('--password', help='Valid project password to login and make requests. Requires username.')
-
-    args = parser.parse_args()
-
-    # login, if needed
-    if args.username and args.password:
-        login(args.project, args.username, args.password)
-
+def runForUser(project, username, password):
+  try:
     # create a new person (record metrics)
+    create_person({"Authorization": "Bearer " + ACCESS_TOKEN})
 
     # get the first page of people (record metrics)
+    get_people({"Authorization": "Bearer " + ACCESS_TOKEN}, 25, 0)
 
     # select a person
 
@@ -197,5 +232,39 @@ if __name__ == "__main__":
     # 1/10 chance to delete person
     #   * delete person (record metrics)
     #   * find and delete count record (if exists)
+  except TokenExpired as e:
+    sc = refresh()
+    if sc != requests.codes.ok:
+      login(project, username, password)
+    
 
-    logger.info("done")
+def runForKey(project, apikey):
+  pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create project usage (tests)")
+    parser.add_argument('project', help='The name of the project to use.')
+    parser.add_argument('--apikey', help='API Key to make authenticated requests, assumes read/write permissions.')
+    parser.add_argument('--username', help='Valid project username to login and make requests. Requires password.')
+    parser.add_argument('--password', help='Valid project password to login and make requests. Requires username.')
+
+    args = parser.parse_args()
+
+    project = args.project
+    username = args.username
+    password = args.password
+
+    # login, if needed
+    if args.username and args.password:
+      login(project, username, password)
+
+      while True:
+        try:
+            runForUser(project, username, password)
+        except Exception as ex:
+            logger.error("exception creating usage: %s", str(ex))
+            traceback.print_tb(ex.__traceback__)
+
+        logger.info("sleeping %s seconds\n", SLEEP_TIME)
+        time.sleep(SLEEP_TIME)
+    logger.info("Done.")
